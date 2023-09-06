2bb5e4d1f53be9186e0bcb62173575f78d445b65
Illei Adrian <illei.adrian@yahoo.com>
2023-09-06 15:09:12 +0200

    Add project files.

---
+++ lab2ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lab2ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int factorial = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                factorial = factorial *i;
+            }
+
+            Console.WriteLine("n factorial este :" + factorial);
+        }
+    }
+}
